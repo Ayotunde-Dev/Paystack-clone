b433da0d3ea192fbb0711c39cd31a56bc559d742
Olusodo <tundeolusodo@gmail.com>
2025-04-09 17:58:35 +0100

    initial commit

---
+++ create.js
@@ -0,0 +1,12 @@
+const digit = 1234;
+const bools = false;
+const text = "letter";
+const nul = null
+const pen = undefined
+
+const digits = String(digit)
+const
+
+const Convertbools = String(bools)
+
+const 
