dd738910a3132d380da8ede74643ccd07441317a
Olusodo <tundeolusodo@gmail.com>
2025-04-10 16:32:08 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,118 @@
+/* function AddTwoAndSeven() {
+
+    return 2+7
+}
+let Num = AddTwoAndSeven
+console.log(String(Num))
+console.log(String(Num).length)
+
+
+let digit = 12345;
+console.log(String(digit));
+console.log(String(digit).length)
+
+const Car = "Toyota"
+console.log(Car);
+console.log(String(Car).length)
+
+
+let symbol = false
+console.log(String (symbol));
+console.log(String(symbol).length)
+
+let def = undefined
+console.log(String(def))
+console.log(String(def).length)
+
+let type = false
+type = true
+console.log(String(type))
+console.log(String(type).length)
+
+const TotalMug = 3
+console.log(String(TotalMug))
+console.log(String(TotalMug).length)
+
+let name =  "Ayo"
+name = "Ayotunde"
+console.log(name)
+console.log(String(name).length)
+
+let bools = false
+console.log(bools)
+console.log(String(bools).length)
+
+const cars = 10
+console.log(String(cars))
+console.log(String(cars).length)
+
+const place = "lagos"
+console.log(place)
+console.log((place).length)
+
+const DataType = null
+console.log(String(DataType))
+console.log(String(DataType).length)
+
+const Shape = "Square"
+console.log(Shape)
+console.log((Shape).length)
+
+
+
+let prime = 2357911
+console.log(String(prime))
+console.log(String(prime).length)
+
+const school = "Yale"
+console.log(school)
+console.log((school).length)
+
+const even = 2468
+console.log(even)
+console.log(String(even).length) 
+
+*/
+
+const numbers = 12345
+const numb = String(numbers)
+console.log(numb)
+
+const value = true
+const val = String(value)
+console .log(val)
+
+const height = 10
+const StrHeight = String(height)
+console.log(StrHeight)
+
+let colour = "red"
+console.log(colour)
+
+let answer = undefined
+let ans = String(undefined)
+console.log(ans)
+
+const nothing = null
+const not = String(null)
+console.log(not)
+
+const Cars = "Mercedes"
+console.log(Cars)
+
+const odd = 13579
+const StrOdd = String(odd)
+console.log(StrOdd)
+
+const alphabet ="abcde"
+console.log(alphabet)
+
+const BoolType = false
+const BoolTypeToString = String (false)
+console.log(BoolTypeToString)
+/*
+const console = ()=>{
+    
+}
+
+*/
